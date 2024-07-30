e45dc2f1f3c44db43cbf59e1cf6b63b14cdbf184
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-07-31 04:48:35 +0600

    task 2 compelete use vegenere.cipher to encode and vigenere.decipher to decode whith keyword password

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const Vigenere = require('caesar-salad').Vigenere;
+
 app.get('/', (req, res) => {
-  return res.send('<div><h3>Enter to address line  "/something" to get echo answer</h3></div>');
+  return res.send('<div><h3>Enter to address line  "/something" to get echo answer </br> or "/encode/:password" to encode password</br> or "/decode/:password" to decode password </h3></div>');
 })
 app.get('/:echoAnswer', (req, res) => {
   return res.send(`${req.params.echoAnswer}`)
 })
 app.get('/encode/:password', (req, res) => {
-  return res.send(`Password encoded! ${req.params.password}`)
+  const password = req.params.password;
+  const encoded = Vigenere.Cipher('password').crypt(password)
+  return res.send(`Password encoded! password: ${encoded}`)
 })
 app.get('/decode/:password', (req, res) => {
-  return res.send(`Password decoded! ${req.params.password}`)
+  const password = req.params.password;
+  const decoded = Vigenere.Decipher('password').crypt(password)
+  return res.send(`Password decoded! password: ${decoded}`)
 })
 
 app.listen(port, () => {
